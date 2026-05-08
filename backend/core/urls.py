50d24c77175fe424bedce0d6f507b0f7c1da647c
Davi Marques <dmarques60@gmail.com>
2026-05-08 20:40:22 -0300

    feat: cria endpoint para listagem de produtos do vendedor logado

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import CadastroVendedorView, CadastroCompradorView, ProdutoListCreateView
+from .views import CadastroVendedorView, CadastroCompradorView, ProdutoListCreateView, MeusProdutosView
 
 urlpatterns = [
     path('cadastro/vendedor/', CadastroVendedorView.as_view(), name='cad_vendedor'),
     path('cadastro/comprador/', CadastroCompradorView.as_view(), name='cad_comprador'),
     path('produtos/', ProdutoListCreateView.as_view(), name='produtos_list_create'),
+    path('meus-produtos/', MeusProdutosView.as_view(), name='meus_produtos'),
 ]
--- backend/core/views.py
+++ backend/core/views.py
@@ -1,3 +1,4 @@
+from rest_framework.permissions import IsAuthenticated
 from rest_framework import generics
 from rest_framework.permissions import AllowAny
 from .models import Vendedor, Comprador, Produto
@@ -17,3 +18,11 @@ class ProdutoListCreateView(generics.ListCreateAPIView):
     queryset = Produto.objects.all()
     serializer_class = ProdutoSerializer
     permission_classes = [AllowAny] # Depois vamos restringir para apenas vendedores logados
+
+class MeusProdutosView(generics.ListAPIView):
+    serializer_class = ProdutoSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        # Retorna apenas os produtos onde o dono é o usuário que está logado
+        return Produto.objects.filter(vendedor__user=self.request.user)
