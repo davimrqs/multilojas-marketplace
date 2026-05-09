91e0fd21eb557b52858f39692d423f6ad7845169
Davi Marques <dmarques60@gmail.com>
2026-05-08 21:06:46 -0300

    feat: automatiza criacao de usuario durante o cadastro de vendedor

--- backend/core/serializers.py
+++ backend/core/serializers.py
@@ -9,15 +9,23 @@ class UserSerializer(serializers.ModelSerializer):
         fields = ['username', 'email', 'password', 'telefone']
 
 class VendedorSerializer(serializers.ModelSerializer):
-    user = UserSerializer()
+    # Campos extras para criar o usuário junto com o vendedor
+    username = serializers.CharField(write_only=True)
+    password = serializers.CharField(write_only=True)
 
     class Meta:
         model = Vendedor
-        fields = ['user', 'nome_loja', 'descricao_loja', 'chave_pix', 'cep_origem']
+        fields = ['id', 'nome_loja', 'cnpj', 'username', 'password']
 
     def create(self, validated_data):
-        user_data = validated_data.pop('user')
-        user = User.objects.create_user(**user_data, is_vendedor=True)
+        # 1. Extrai os dados de login
+        username = validated_data.pop('username')
+        password = validated_data.pop('password')
+        
+        # 2. Cria o Usuário no sistema de autenticação
+        user = User.objects.create_user(username=username, password=password)
+        
+        # 3. Cria o Vendedor vinculado a esse novo usuário
         vendedor = Vendedor.objects.create(user=user, **validated_data)
         return vendedor
 
@@ -35,8 +43,10 @@ class CompradorSerializer(serializers.ModelSerializer):
         return comprador
     
 class ProdutoSerializer(serializers.ModelSerializer):
+    # Adicione isso para o Serializer não exigir o ID no formulário
     vendedor_nome = serializers.ReadOnlyField(source='vendedor.nome_loja')
-
+    
     class Meta:
         model = Produto
-        fields = ['id', 'vendedor', 'vendedor_nome', 'nome', 'descricao', 'preco', 'estoque', 'imagem', 'criado_em']
+        fields = '__all__'
+        read_only_fields = ['vendedor'] # <--- ISSO AQUI É A CHAVE
